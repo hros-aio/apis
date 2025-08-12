287327e36ab19b9a5f33b36b0fbf3c8be5f819ad
Ren0503 <illuminati0440@gmail.com>
2025-08-12 23:04:08 +0700

    feat: update flow sync when create tenant

--- libs/psql/common/tenant/tenant_model.go
+++ libs/psql/common/tenant/tenant_model.go
@@ -4,7 +4,7 @@ import "github.com/hros-aio/apis/libs/psql/common/base"
 
 type ContactPerson struct {
 	ContactName  string `json:"name" example:"Renil"`
-	ContactEmail string ` json:"email" example:"renil@gmail.com" validate:"isEmail"`
+	ContactEmail string `json:"email" example:"renil@gmail.com" validate:"isEmail"`
 	ContactPhone string `json:"phone" example:"012345678"`
 }
 
--- libs/saga/messages/tenat_message.go
+++ libs/saga/messages/tenat_message.go
@@ -2,11 +2,17 @@ package messages
 
 import "time"
 
+type ContactPerson struct {
+	ContactName  string `json:"name"`
+	ContactEmail string `json:"email"`
+	ContactPhone string `json:"phone"`
+}
+
 type TenantCreatedPayload struct {
-	Id        string     `json:"id,omitempty"`
-	Name      string     `json:"name.omitempty"`
-	Email     string     `json:"email"`
-	CreatedAt *time.Time `json:"createdAt,omitempty"`
-	TenantId  string     `json:"tenantId,omitempty"`
-	Domain    string     `json:"domain,omitempty"`
+	Id        string        `json:"id,omitempty"`
+	Name      string        `json:"name.omitempty"`
+	CreatedAt *time.Time    `json:"createdAt,omitempty"`
+	TenantId  string        `json:"tenantId,omitempty"`
+	Domain    string        `json:"domain,omitempty"`
+	Contact   ContactPerson `json:"contact,omitzero"`
 }
